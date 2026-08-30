ac18acb4eb3e6b67decc96310014727b28bd8d36
illya-klus <illyaklus2@gmail.com>
2026-08-30 23:18:03 +0300

    fix(configs): remembering and basic resetup

--- services/gateway/src/app.module.ts
+++ services/gateway/src/app.module.ts
@@ -6,39 +6,14 @@ import {
 } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 import { HealthModule } from "./modules/health/health.module";
 import { RequestIdMiddleware } from "./common/middleware/request-ids.middleware";
 import { RequestsLoggerMiddleware } from "./common/middleware/requests-loger.middleware";
 import { AuthModule } from "./modules/auth/auth.module";
-import { ClientsModule, Transport } from "@nestjs/microservices";
-import { join } from "node:path";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    HealthModule,
-    AuthModule,
-    ClientsModule.registerAsync([
-      {
-        imports: [ConfigModule],
-        name: "AUTH_PACKAGE",
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.GRPC,
-          options: {
-            package: "auth.v1",
-            protoPath: join(
-              process.cwd(),
-              "../../packages/shared-protos/proto/auth.proto",
-            ),
-            url: configService.getOrThrow<string>("AUTH_GRPC_URL"),
-          },
-        }),
-      },
-    ]),
-  ],
-
+  imports: [ConfigModule.forRoot({ isGlobal: true }), HealthModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
